a7e7632f257ebdc1160b11e32c0023c8b296396e
khoi-h-tran <khoi.huynh.tran@gmail.com>
2022-03-05 14:53:15 -0500

    Organized App Module Imports

--- App.Main.Bet/src/app/app.module.ts
+++ App.Main.Bet/src/app/app.module.ts
@@ -1,14 +1,19 @@
+// Angular
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule, Title } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// NGRX
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { NavbarComponent } from './features/navbar/navbar.component';
 
+// environemnt file
+import { environment } from '../environments/environment';
+
+// PrimeNG
 import { AccordionModule } from 'primeng/accordion';
 import { AvatarModule } from 'primeng/avatar';
 import { CardModule } from 'primeng/card';
@@ -17,13 +22,13 @@ import { RadioButtonModule } from 'primeng/radiobutton';
 import { TabViewModule } from 'primeng/tabview';
 import { TabMenuModule } from 'primeng/tabmenu';
 
+// Components
 import { BetPageComponent } from './features/bet-page/bet-page.component';
-import { StatisticsPageComponent } from './features/statistics-page/statistics-page.component';
-
 import { betReducer } from './app-state/reducers/bet.reducer';
-import { HttpClientModule } from '@angular/common/http';
 import { FightEventCardComponent } from './features/bet-page/fight-event-card/fight-event-card.component';
 import { FightEventComponent } from './features/bet-page/fight-event-card/fight-event/fight-event.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
+import { StatisticsPageComponent } from './features/statistics-page/statistics-page.component';
 
 @NgModule({
   declarations: [
